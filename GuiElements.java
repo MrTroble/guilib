79337783f7da8b612ec452083c3555102f09dedd
MrTroble <nifroh56@gmail.com>
2021-12-01 00:22:53 +0100

    Fixed issues

--- GuiElements.java
+++ GuiElements.java
@@ -802,30 +802,48 @@ public class GuiElements {
 		middle.setBounds(Minecraft.getMinecraft().fontRenderer.getStringWidth(property.getLocalizedName()) + 20, 20);
 
 		final UICheckBox middleButton = new UICheckBox(property.getName());
+		final UIClickable clickable = new UIClickable(e -> {
+			middleButton.setChecked(!middleButton.isChecked());
+		});
 		middleButton.setOnChange(consumer);
 		middleButton.setText(property.getLocalizedName());
 		middle.add(middleButton);
+		middle.add(clickable);
 		return middle;
 	}
 
 	public static UIEntity createEnumElement(IIntegerable<?> property, IntConsumer consumer) {
+		return createEnumElement(property, consumer, property.getMaxWidth(Minecraft.getMinecraft().fontRenderer) + 8);
+	}
+	
+	public static UIEntity createEnumElement(IIntegerable<?> property, IntConsumer consumer, int minWidth) {
 		final UIEntity middle = new UIEntity();
-		middle.setBounds(property.getMaxWidth(Minecraft.getMinecraft().fontRenderer), 20);
+		middle.setBounds(minWidth, 20);
+
+		final UIButton leftButton = new UIButton("<");
+		final UIButton rightButton = new UIButton(">");
 
 		final UIButton middleButton = new UIButton(property.getNamedObj(0));
 		final UIEnumerable enumerable = new UIEnumerable(consumer, property.count(), property.getName());
-		enumerable.setOnChange(consumer.andThen(in -> middleButton.setText(property.getNamedObj(in))));
+		enumerable.setOnChange(consumer.andThen(in -> {
+			middleButton.setText(property.getNamedObj(in));
+			rightButton.setEnabled(true);
+			leftButton.setEnabled(true);
+			if (in <= enumerable.getMin())
+				leftButton.setEnabled(false);
+			if (in >= enumerable.getMax() - 1)
+				rightButton.setEnabled(false);
+		}));
 		middle.add(middleButton);
 		middle.add(enumerable);
 
 		final UIEntity left = new UIEntity();
 		left.setBounds(20, 20);
-
-		final UIButton leftButton = new UIButton("<");
+		
 		final UIClickable leftclickable = new UIClickable(e -> {
 			final int id = enumerable.getIndex();
 			final int min = enumerable.getMin();
-			if (id == min)
+			if (id <= min)
 				return;
 			enumerable.setIndex(id - 1);
 		});
@@ -835,13 +853,12 @@ public class GuiElements {
 		final UIEntity right = new UIEntity();
 		right.setBounds(20, 20);
 
-		final UIButton rightButton = new UIButton(">");
 		final UIClickable rightclickable = new UIClickable(e -> {
 			final int id = enumerable.getIndex();
-			final int min = enumerable.getMin();
-			if (id == min)
+			final int max = enumerable.getMax() - 1;
+			if (id >= max)
 				return;
-			enumerable.setIndex(id - 1);
+			enumerable.setIndex(id + 1);
 		});
 		right.add(rightButton);
 		right.add(rightclickable);
