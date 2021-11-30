12c7b974ab98d3bad40dfe8faca76d8e439c8551
FGBxRamel <kretschmer.raziel@gmail.com>
2021-11-30 01:09:01 +0100

    Added UIColor class

---
+++ UIColor.java
@@ -0,0 +1,32 @@
+package eu.gir.girsignals.guis.guilib;
+
+import eu.gir.girsignals.guis.guilib.GuiElements.UIComponent;
+import net.minecraftforge.fml.client.config.GuiUtils;
+
+public class UIColor extends UIComponent {
+
+	private int color;
+
+	public UIColor(int color) {
+		this.setColor(color);
+	}
+
+	@Override
+	public void draw(int mouseX, int mouseY) {
+		GuiUtils.drawGradientRect(0, 0, 0, parent.getWidth(), parent.getHeight(), this.color, this.color);
+	}
+
+	@Override
+	public void update() {
+
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+}
