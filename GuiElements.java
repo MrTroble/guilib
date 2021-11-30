728db1ee57af67e517e44d51aac689356e2334a5
FGBxRamel <kretschmer.raziel@gmail.com>
2021-11-30 16:39:05 +0100

    Reworked UILabel

--- GuiElements.java
+++ GuiElements.java
@@ -9,7 +9,6 @@ import org.lwjgl.input.Keyboard;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.GuiLabel;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.nbt.NBTTagCompound;
@@ -427,31 +426,48 @@ public class GuiElements {
 
 	public static class UILabel extends UIComponent {
 
-		public GuiLabel label;
+		public static final int DEFAULT_STRING_COLOR = 4210752;
+		private String string;
+		private int stringColor;
+		private FontRenderer renderer;
 
-		public UILabel() {
-			label = new GuiLabel(Minecraft.getMinecraft().fontRenderer, IDENTIFIER, 0, 0, 200, 20, 0xFFFFFFF);
+		public UILabel(String text) {
+			this.string = text;
+			this.stringColor = DEFAULT_STRING_COLOR;
+			this.renderer = Minecraft.getMinecraft().fontRenderer;
 		}
 
 		@Override
 		public void draw(int mouseX, int mouseY) {
-			label.drawLabel(Minecraft.getMinecraft(), mouseX, mouseY);
+			if (this.visible) {
+				GlStateManager.enableBlend();
+				GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA,
+						GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE,
+						GlStateManager.DestFactor.ZERO);
+				renderer.drawString(string, 0, 0, stringColor);
+			}
 		}
 
 		@Override
 		public void update() {
-			label.x = parent.x;
-			label.y = parent.y;
+			this.parent.height = renderer.FONT_HEIGHT;
+			this.parent.width = renderer.getStringWidth(string);
 		}
 
-		@Override
-		public void setVisible(boolean visible) {
-			label.visible = visible;
+		public String getText() {
+			return string;
 		}
 
-		@Override
-		public boolean isVisible() {
-			return label.visible;
+		public void setText(String string) {
+			this.string = string;
+		}
+
+		public int getTextColor() {
+			return stringColor;
+		}
+
+		public void setTextColor(int stringColor) {
+			this.stringColor = stringColor;
 		}
 
 	}
