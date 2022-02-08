d8e20b3d5448951e47a9b98bf7e3f1bd98777dfc
MrTroble <nifroh56@gmail.com>
2022-02-08 13:30:46 +0100

    Removed client only on auto sync

--- UIAutoSync.java
+++ UIAutoSync.java
@@ -1,10 +1,7 @@
 package eu.gir.girsignals.guis.guilib;
 
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public interface UIAutoSync {
 	
 	public void write(final NBTTagCompound compound);
