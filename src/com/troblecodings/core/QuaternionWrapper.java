81cf5c61e162b5e36254984b547134a363c53346
Cedric <cedric.hathaway05@gmail.com>
2025-03-11 18:31:14 +0100

    fix: quaternion translation

--- src/com/troblecodings/core/QuaternionWrapper.java
+++ src/com/troblecodings/core/QuaternionWrapper.java
@@ -5,23 +5,25 @@ import net.minecraft.util.math.vector.Vector3f;
 
 public class QuaternionWrapper {
 
-    public static Quaternion fromXYZ(final float rotateX, final float rotateY,
-            final float rotateZ) {
-        final Quaternion quaternion = Quaternion.ONE.copy();
-        quaternion.mul(new Quaternion((float) Math.sin(rotateX / 2.0F), 0.0F, 0.0F,
-                (float) Math.cos(rotateX / 2.0F)));
-        quaternion.mul(new Quaternion(0.0F, (float) Math.sin(rotateY / 2.0F), 0.0F,
-                (float) Math.cos(rotateY / 2.0F)));
-        quaternion.mul(new Quaternion(0.0F, 0.0F, (float) Math.sin(rotateZ / 2.0F),
-                (float) Math.cos(rotateZ / 2.0F)));
+    public static final Quaternion ONE = new Quaternion(0.0F, 0.0F, 0.0F, 1.0F);
+
+    public static Quaternion fromXYZ(final float p_175229_, final float p_175230_,
+            final float p_175231_) {
+        Quaternion quaternion = ONE.copy();
+        quaternion.mul(new Quaternion((float) Math.sin((double) (p_175229_ / 2.0F)), 0.0F, 0.0F,
+                (float) Math.cos((double) (p_175229_ / 2.0F))));
+        quaternion.mul(new Quaternion(0.0F, (float) Math.sin((double) (p_175230_ / 2.0F)), 0.0F,
+                (float) Math.cos((double) (p_175230_ / 2.0F))));
+        quaternion.mul(new Quaternion(0.0F, 0.0F, (float) Math.sin((double) (p_175231_ / 2.0F)),
+                (float) Math.cos((double) (p_175231_ / 2.0F))));
         return quaternion;
     }
 
     public static Vector3f toYXZ(final Quaternion q) {
-        float f = q.i() * q.i();
-        float f1 = q.j() * q.j();
-        float f2 = q.k() * q.k();
-        float f3 = q.r() * q.r();
+        float f = q.r() * q.r();
+        float f1 = q.i() * q.i();
+        float f2 = q.j() * q.j();
+        float f3 = q.k() * q.k();
         float f4 = f + f1 + f2 + f3;
         float f5 = 2.0F * q.r() * q.i() - 2.0F * q.j() * q.k();
         float f6 = (float) Math.asin((double) (f5 / f4));
