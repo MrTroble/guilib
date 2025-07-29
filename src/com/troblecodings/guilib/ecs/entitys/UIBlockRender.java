8757e7a89598c5fb0bb127aca7225289cd2ffd29
Cedric <cedric.hathaway05@gmail.com>
2025-07-30 00:16:00 +0200

    fix: Nullpointer Issue

--- src/com/troblecodings/guilib/ecs/entitys/UIBlockRender.java
+++ src/com/troblecodings/guilib/ecs/entitys/UIBlockRender.java
@@ -35,6 +35,8 @@ public class UIBlockRender extends UIComponent {
 
     @Override
     public void draw(final DrawInfo info) {
+        if (info == null)
+            return;
         info.applyTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
         GlStateManager.enableRescaleNormal();
         info.depthOn();
